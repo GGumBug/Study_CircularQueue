a1f48b893f136eec7f546a6095031f247c2a37bc
GGumBug <alsrl7538@gmail.com>
2023-04-02 01:00:38 +0900

    CircularQueue

---
+++ CircularQueue/CircularQueue.cs
@@ -0,0 +1,83 @@
+﻿using System;
+namespace CircularQueue
+{
+	public class CircularQueue<T>
+	{
+        private T[] elements;
+        private int front, rear;
+
+        public CircularQueue(int size)
+        {
+            elements = new T[size];
+            front = rear = -1;
+        }
+
+        public bool IsEmpty()
+        {
+            return front == -1 && rear == -1;
+        }
+
+        public bool IsFull()
+        {
+            return (rear + 1) % elements.Length == front;
+        }
+
+        public void EnQueue(T item)
+        {
+            if (IsFull())
+            {
+                // 에러메세지를 던지다
+                throw new Exception("Queue is empty");
+            }
+            else if (IsEmpty())
+            {
+                front = rear = 0;
+            }
+            else
+            {
+                rear = (rear + 1) % elements.Length;
+            }
+
+            elements[rear] = item;
+
+        }
+
+        public T DeQueue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue underflow");
+
+                return default(T);
+            }
+            else if (front == rear)
+            {
+                T item = elements[front];
+                front = rear = -1;
+                return item;
+            }
+            else
+            {
+                T item = elements[front];
+                front = (front + 1) % elements.Length;
+                return item;
+            }
+        }
+
+        public void Print()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("Queue empty");
+                return;
+            }
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                Console.Write($"{elements[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
---
+++ CircularQueue/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CircularQueue
+{
+	public class Program
+	{
+        static void Main(string[] args)
+        {
+            CircularQueue<int> circularQueue = new CircularQueue<int>(5);
+
+            circularQueue.EnQueue(1);
+            circularQueue.EnQueue(2);
+            circularQueue.EnQueue(3);
+            circularQueue.EnQueue(4);
+            circularQueue.EnQueue(5);
+        }
+	}
+}
+
